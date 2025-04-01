735712983e66c5584bcccd3b93243a57ba32338e
Denissevar <dvargasvel@gmail.com>
2025-04-01 21:40:30 +0200

    1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Buena suerte
+
+//which inputs I need//
+
+const inputPounds = document.querySelector(`#input`);
+const spanKg = document.querySelector("result");
+//* agregar evetn listener//
+
+inputPounds.addEventListener("input", function (event) {
+    const pounds = parseFloat(event.target.value);   
+
+    //1 pound son 0.4
+    // hay que multiplicar el valor de variable pounds por 0.4535
+
+    if (!isNaN(pounds)) {
+spanKg.textContent = "";
+e = pounds * 0453592
+    })
+})
+
